2e048c45d303747e45bdcd43da42c675567a191c
RasulArslanov <rasularslanoov@gmail.com>
2020-08-18 14:47:25 +0500

    Added remove photo handler constructor

--- api/src/Domain/Todo/UseCase/Person/RemovePhoto/Handler.php
+++ api/src/Domain/Todo/UseCase/Person/RemovePhoto/Handler.php
@@ -15,6 +15,19 @@ final class Handler
     private PhotoRemover $remover;
     private FlusherInterface $flusher;
 
+    /**
+     * Handler constructor.
+     * @param PersonRepository $persons
+     * @param PhotoRemover $remover
+     * @param FlusherInterface $flusher
+     */
+    public function __construct(PersonRepository $persons, PhotoRemover $remover, FlusherInterface $flusher)
+    {
+        $this->persons = $persons;
+        $this->remover = $remover;
+        $this->flusher = $flusher;
+    }
+
     public function handle(Command $command): void
     {
         $person = $this->persons->getById(new Id($command->id));
