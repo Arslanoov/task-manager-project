8b414558ce70015fe4d2bf7229e74d3bda0974b5
RasulArslanov <rasularslanoov@gmail.com>
2020-08-30 19:40:58 +0500

    Installed SwiftMailer

---
+++ api/config/params/mail.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'mail' => [
+        'host' => getenv('MAILER_HOST'),
+        'port' => getenv('MAILER_PORT'),
+        'user' => getenv('MAILER_USER'),
+        'password' => getenv('MAILER_PASSWORD'),
+        'encryption' => getenv('MAILER_ENCRYPTION')
+    ]
+];
