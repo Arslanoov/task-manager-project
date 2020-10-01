a3bc7f65863c74eae8b5ba5304af7eb55c84abe1
RasulArslanov <rasularslanoov@gmail.com>
2020-10-01 22:01:34 +0500

    Try fix storage 5

--- api/config/params/prod/service.php
+++ api/config/params/prod/service.php
@@ -2,7 +2,7 @@
 
 return [
     'service' => [
-        'background_photo_path' => dirname(__DIR__, 3) . '/storage/public/photos',
+        'background_photo_path' => '/var/www/api/storage/public/photos',
         'background_photo_url' => getenv('STORAGE_URL') . '/photos'
     ]
 ];
