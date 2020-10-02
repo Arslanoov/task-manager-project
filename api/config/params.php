102f548a394b1491ac55cb87ecbff792446b3af9
RasulArslanov <rasularslanoov@gmail.com>
2020-10-02 07:17:15 +0500

    Fixed params

--- api/config/params.php
+++ api/config/params.php
@@ -6,8 +6,8 @@ use Laminas\ConfigAggregator\ConfigAggregator;
 use Laminas\ConfigAggregator\PhpFileProvider;
 
 $configAggregator = new ConfigAggregator([
-    new PhpFileProvider(__DIR__ . '/params/*.php'),
-    new PhpFileProvider(__DIR__ . '/params/' . (getenv('ENV') ?? 'prod') . '/*.php')
+    new PhpFileProvider(__DIR__ . '/params/*-common.php'),
+    new PhpFileProvider(__DIR__ . '/params/*-' . (getenv('ENV') ?: 'prod') . '.php')
 ]);
 
 return $configAggregator->getMergedConfig();
