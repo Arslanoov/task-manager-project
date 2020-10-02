5a76078629bf0eaf353e57bcb2c0d5434530768f
RasulArslanov <rasularslanoov@gmail.com>
2020-10-02 07:24:50 +0500

    Fixed service params 2

--- api/config/params.php
+++ api/config/params.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 use Laminas\ConfigAggregator\ConfigAggregator;
 use Laminas\ConfigAggregator\PhpFileProvider;
 
+define('ENV', getenv('ENV') ?: 'prod');
+
 $configAggregator = new ConfigAggregator([
     new PhpFileProvider(__DIR__ . '/params/*-common.php'),
-    new PhpFileProvider(__DIR__ . '/params/*-' . (getenv('ENV') ?: 'prod') . '.php')
+    new PhpFileProvider(__DIR__ . '/params/' . ENV . '/*-' . ENV . '.php')
 ]);
 
 return $configAggregator->getMergedConfig();
